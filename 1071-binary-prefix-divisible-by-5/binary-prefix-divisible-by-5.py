a0a3303be3d352217b2f73dc2777815e5ea0ec2f
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-06-05 19:36:37 +0530

    Time: 4 ms (77.52%) | Memory: 19.3 MB (19.38%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
@@ -2,13 +2,19 @@ import sys
 sys.set_int_max_str_digits(20000)
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        l=[]
-        s=''
-        for i in nums:
-            s+=str(i)
-            if int(s,2)%5==0:
-                l.append(True)
-            else:
-                l.append(False)
-        return l
+        # l=[]
+        # s=''
+        # for i in nums:
+        #     s+=str(i)
+        #     if int(s,2)%5==0:
+        #         l.append(True)
+        #     else:
+        #         l.append(False)
+        # return l
 
+        l=[]
+        k=0
+        for i in nums:
+            k=(k*2+i)%5
+            l.append(k==0)
+        return l
