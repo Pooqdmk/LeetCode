2688d99ba882194a18ea0dcfdd5b171ee8eca220
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-06-17 22:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (17.27%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,35 +1,15 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        # cnt=0
+        # r=l=d=u=0
         # for i in moves:
-        #     if i=="U" or i=="R":
-        #         cnt+=1
+        #     if i=="R":
+        #         r+=1
+        #     elif i=="L":
+        #         l+=1
+        #     elif i=="U":
+        #         u+=1
         #     else:
-        #         cnt-=1
-        # return True if cnt==0 else False
+        #         d+=1
+        # return (r==l) and (u==d)
 
-        # stack=[]
-
-        # for i in moves:
-        #     if stack and stack[-1]=="R" and i=="L":
-        #         stack.pop()
-        #     elif stack and stack[-1]=="L" and i=="R":
-        #         stack.pop()
-        #     elif stack and stack[-1]=="D" and i=="U":
-        #         stack.pop()
-        #     elif stack and stack[-1]=="U" and i=="D":
-        #         stack.pop()
-        #     else:
-        #         stack.append(i)
-        # return len(stack)==0
-        r=l=d=u=0
-        for i in moves:
-            if i=="R":
-                r+=1
-            elif i=="L":
-                l+=1
-            elif i=="U":
-                u+=1
-            else:
-                d+=1
-        return (r==l) and (u==d)
+        return moves.count("R")==moves.count("L") and moves.count("D")==moves.count("U")
