d09cd3700d945abf7bc9b2f0a8f25b233c765177
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-06-17 19:42:47 +0530

    Time: 12 ms (60.78%) | Memory: 21.4 MB (40.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        n=len(strs)
+        # n=len(strs)
         
         d=defaultdict(list)
         for i in strs:
