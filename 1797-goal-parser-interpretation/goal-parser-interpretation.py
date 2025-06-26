cf4bf48e8335f3ff5efdac1f2fbc41f817975669
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-06-26 22:32:27 +0530

    Time: 36 ms (72.50%) | Memory: 17.7 MB (42.17%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,15 +1,16 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        res=''
-        i=0
-        while i < len(command):
-            if command[i] == 'G':
-                res+='G'
-                i+=1
-            elif command[i] == '(' and command[i+1] == ')':
-                res+='o'
-                i+=2
-            else:
-                res+='al'
-                i+=4
-        return res
+        # res=''
+        # i=0
+        # while i < len(command):
+        #     if command[i] == 'G':
+        #         res+='G'
+        #         i+=1
+        #     elif command[i] == '(' and command[i+1] == ')':
+        #         res+='o'
+        #         i+=2
+        #     else:
+        #         res+='al'
+        #         i+=4
+        # return res
+        return command.replace('()' , 'o').replace('(al)','al')
