56e066024ac4385a739379a93be95d36fad82986
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-06-28 20:17:25 +0530

    Time: 3 ms (78.06%) | Memory: 22 MB (33.84%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives,tens,twen=0,0,0
+        for i in bills:
+            if i==5:
+                fives+=1
+            elif i==10 and fives >=1:
+                fives-=1
+                tens+=1
+            elif i==20 and fives>=1 and tens>=1:
+                tens-=1
+                fives-=1
+                twen+=1
+            elif i==20 and fives >=3 :
+                fives-=3
+                twen+=1
+            
+            else:
+                return False
+        return True
+
+        
