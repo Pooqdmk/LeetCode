0c976011181d50819a12738fee79d894494b13bc
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-04 20:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.89%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -2,10 +2,5 @@ class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         d=Counter(nums)
 
-        # s=sorted(d.items(),key=lambda x:x[1])
-        # res=[]
-        # for i in s:
-        #     res.append([i[0]]*i[1])
-        # return sum(res,[])
         nums.sort(key=lambda x:(d[x],-x))
         return nums
