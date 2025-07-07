4f9e87b99280d66bcb5d91b6585bc27df07dcd5d
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-07 23:19:55 +0530

    Time: 7 ms (55.32%) | Memory: 17.8 MB (44.18%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.py
+++ 299-bulls-and-cows/bulls-and-cows.py
@@ -1,19 +1,31 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        cnt=0
-        cows=0
-        s=list(secret)
-        l=list(guess)
+        # cnt=0
+        # cows=0
+        # s=list(secret)
+        # l=list(guess)
+        # n=len(secret)
+        # for i in range(n):
+        #     if secret[i] == guess[i]:
+        #         cnt+=1
+        #         s.remove(secret[i])
+        #         l.remove(guess[i])
+
+        # for i in l:
+        #     if i in s:
+        #         cows+=1
+        #         s.remove(i)
+        d=Counter(secret)
+        cnt,cows=0,0
         n=len(secret)
         for i in range(n):
-            if secret[i] == guess[i]:
+            if secret[i]==guess[i]:
                 cnt+=1
-                s.remove(secret[i])
-                l.remove(guess[i])
+                d[secret[i]]-=1 
 
-        for i in l:
-            if i in s:
+        for i in range(n):
+            if secret[i]!=guess[i] and d[guess[i]] > 0:
                 cows+=1
-                s.remove(i)
+                d[guess[i]]-=1
 
         return str(cnt)+'A'+str(cows)+'B'
