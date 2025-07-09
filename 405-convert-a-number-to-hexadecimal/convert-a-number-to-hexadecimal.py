3b11b1b78fc26d16747b8b0cbd3ffbb07057d540
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-09 08:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.10%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -1,8 +1,6 @@
 class Solution:
     def toHex(self, num: int) -> str:
-        if num>0:
+        if num>=0:
             return hex(num)[2:]
-        if num==0:
-            return '0'
         n=(1 << 32) + num
         return hex(n)[2:]
