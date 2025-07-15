035c92115ab255953e42dcea6377a48a97941c90
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-15 12:21:38 +0530

    Time: 8 ms (15.72%) | Memory: 17.8 MB (63.14%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        mx=-1
+        for i in range(1,len(s)):
+            mx=max(mx,s[:i].count('0') + s[i:].count('1'))
+
+        return mx
