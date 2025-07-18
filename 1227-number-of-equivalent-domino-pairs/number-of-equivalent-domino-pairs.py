8e1b1d113a116d3be64a493970cec9043ea3b22e
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-18 11:13:57 +0530

    Time: 7 ms (81.81%) | Memory: 24.1 MB (98.74%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
@@ -3,13 +3,15 @@ class Solution:
         d={}
 
         for i in dominoes:
-            if tuple(i) not in d and tuple(i[::-1]) not in d:
-                d[tuple(i)]=1
-            else:
-                if tuple(i) in d:
-                    d[tuple(i)]+=1
-                else:
-                    d[tuple(i[::-1])]+=1
+            # if tuple(i) not in d and tuple(i[::-1]) not in d:
+            #     d[tuple(i)]=1
+            # else:
+            #     if tuple(i) in d:
+            #         d[tuple(i)]+=1
+            #     else:
+            #         d[tuple(i[::-1])]+=1
+            key=tuple(sorted(i))
+            d[key]=d.get(key,0)+1
 
         cnt=0
         for key,val in d.items():
