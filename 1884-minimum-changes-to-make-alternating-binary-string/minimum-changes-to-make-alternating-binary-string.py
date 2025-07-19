27e01fa21813327f0a570ea9b4f87f6e5873ae55
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-19 20:23:11 +0530

    Time: 11 ms (59.36%) | Memory: 18 MB (41.91%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,24 +1,40 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        cnt1=0
-        cnt2=0
+        # cnt1=0
+        # cnt2=0
+        # n=len(s)
+        # res1=['0']*n
+        # res2=['1']*n
+        # for i in range(n-1):
+        #     if res1[i]=='0':
+        #         res1[i+1]='1'
+        #     else:
+        #         res1[i+1]='0'
+
+        #     if res2[i]=='0':
+        #         res2[i+1]='1'
+        #     else:
+        #         res2[i+1]='0'
+
+        # for i in range(n):
+        #     if s[i]!=res1[i]:
+        #         cnt1+=1
+        #     if s[i]!=res2[i]:
+        #         cnt2+=1
+        # return min(cnt1,cnt2)
+        a,b=0,0
         n=len(s)
-        res1=['0']*n
-        res2=['1']*n
-        for i in range(n-1):
-            if res1[i]=='0':
-                res1[i+1]='1'
-            else:
-                res1[i+1]='0'
-
-            if res2[i]=='0':
-                res2[i+1]='1'
-            else:
-                res2[i+1]='0'
-
+        res1,res2=['0']*n,['0']*n
         for i in range(n):
+            if i%2==0:
+                res1[i]='0'
+                res2[i]='1'
+            else:
+                res1[i]='1'
+                res2[i]='0'
+            
             if s[i]!=res1[i]:
-                cnt1+=1
+                a+=1
             if s[i]!=res2[i]:
-                cnt2+=1
-        return min(cnt1,cnt2)
+                b+=1
+        return min(a,b)
