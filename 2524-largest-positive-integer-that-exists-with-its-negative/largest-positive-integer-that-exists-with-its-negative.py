6b745628dd9bdc7b013a8723910670f5f56ed156
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-07-20 21:11:10 +0530

    Time: 13 ms (30.84%) | Memory: 17.9 MB (46.37%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
@@ -1,8 +1,8 @@
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        mx=-1
-
+        # mx=-1
+        nums.sort(reverse=True)
         for i in nums:
             if i>0 and -i in nums:
-                mx=max(mx,i)
-        return mx
+                return i
+        return -1
