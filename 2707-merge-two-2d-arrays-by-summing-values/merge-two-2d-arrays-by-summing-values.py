93e54fd756cecd4f739e714d395ddc8c0be10085
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-08-02 18:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (60.79%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -3,17 +3,21 @@ class Solution:
         d={i[0]:i[1] for i in nums1}
         l={i[0]:i[1] for i in nums2}
 
-        nums=list(set([i[0] for i in nums1] + [i[0] for i in nums2]))
-        res={}
-        for i in nums:
-            if i in d and i in l:
-                res[i]=d[i]+l[i]
-            elif i in d:
-                res[i]=d[i]
-            else:
-                res[i]=l[i]
+        nums=set(d.keys()).union(set(l.keys()))
+        res=[]
+        # for i in nums:
+        #     if i in d and i in l:
+        #         res[i]=d[i]+l[i]
+        #     elif i in d:
+        #         res[i]=d[i]
+        #     else:
+        #         res[i]=l[i]
         
-        res=list(sorted(res.items(),key=lambda x:x[0]))
-        for i in range(len(res)):
-            res[i]=list(res[i])
+        # res=list(sorted(res.items(),key=lambda x:x[0]))
+        # for i in range(len(res)):
+        #     res[i]=list(res[i])
+        # return res
+
+        for i in sorted(nums):
+            res.append([i,d.get(i,0)+l.get(i,0)])
         return res
