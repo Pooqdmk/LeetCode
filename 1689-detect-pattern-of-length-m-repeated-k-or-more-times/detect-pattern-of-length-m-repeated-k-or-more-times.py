64d120cb533ede3762e34783b4637103175a81d6
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-08-06 21:28:06 +0530

    Time: 19 ms (4.69%) | Memory: 17.6 MB (88.28%) - LeetSync

--- 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.py
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.py
@@ -4,9 +4,6 @@ class Solution:
         for i in range(n-m+1):
             pattern=arr[i:i+m]
             p=pattern*k
-            
-            # if ''.join([str(i) for i in p]) in ''.join([str(i) for i in arr]):
-            #     return True
 
             for j in range(0,n-len(p)+1):
                 if arr[j:j+len(p)] == p:
