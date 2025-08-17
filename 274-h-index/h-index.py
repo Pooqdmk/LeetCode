136ade699ba70d214a1f97d34c6ab3f3ec31f350
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-08-17 09:42:24 +0530

    Time: 3 ms (23.54%) | Memory: 18.1 MB (40.39%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hIndex(self, c: List[int]) -> int:
+        c.sort(reverse = True)
+        n=len(c)
+        i=n
+        while i > 0:
+            cnt=0
+            for j in c:
+                if j >= i:
+                    cnt+=1
+                else:
+                    break
+            if cnt >= i:
+                return i
+            i-=1
+        return 0
+
