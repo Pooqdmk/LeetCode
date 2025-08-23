7044c24ca4ab447cf8a54eae49267c43c4aac6fa
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-08-23 11:15:06 +0530

    Time: 2653 ms (5.01%) | Memory: 19.3 MB (86.39%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        res=[]
+        
         n=len(nums)
+        res=[-1]*n
         for i in range(n):
-            found=False
+            
             for j in range(n):
                 if nums[(j+i+1) % n] > nums[i]:
-                    found=True
-                    res.append(nums[(j+i+1)%n])
+                    res[i]=(nums[(j+i+1)%n])
                     break
-            if not found:
-                res.append(-1)
+            
         return res
