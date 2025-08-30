89000401c21b33d59ae95a48ca9f5a28bf7f0e0a
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-08-30 10:25:59 +0530

    Time: 3 ms (81.81%) | Memory: 17.8 MB (69.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,35 +1,18 @@
 class Solution:
     def isValidSudoku(self, b: List[List[str]]) -> bool:
-        n=len(b)
-        m=len(b[0])
-        
-        for i in range(n):
-            s=set()
-            for j in range(m):
-                if b[i][j]!='.':
-                    if b[i][j] not in s:
-                        s.add(b[i][j])
-                    else:
-                        return False        
-        for j in range(m):
-            s=set()
-            for i in range(n):
-                if b[i][j] != '.':
-                    if b[i][j] not in s:
-                        s.add(b[i][j])
-                    else:
-                        return False
-        
-        d=defaultdict(set)
+        r=defaultdict(set)
+        c=defaultdict(set)
+        sq=defaultdict(set)
 
-        for i in range(n):
-            for j in range(m):
-                if b[i][j]!='.':
-                    a=i//3 
-                    c=j//3
-                    if b[i][j] not in d[(a,c)]:
-                        d[(a,c)].add(b[i][j])
-                    else:
-                        return False
+        for i in range(9):
+            for j in range(9):
+                if b[i][j] == '.':
+                    continue
+                
+                if b[i][j] in r[i] or b[i][j] in c[j] or b[i][j] in sq[(i//3,j//3)]:
+                    return False
+                
+                r[i].add(b[i][j])
+                c[j].add(b[i][j])
+                sq[(i//3,j//3)].add(b[i][j])
         return True
-
