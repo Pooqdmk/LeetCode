92e45e249fc9c68d79a8d5c94f49193f2629fb9b
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-03 20:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.78%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -1,6 +1,4 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort()
-
-        return nums2[0] - nums1[0]
+        m,n = min(nums1),min(nums2)
+        return n-m
