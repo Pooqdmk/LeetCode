13ff79a9f0157d77b30d6453184607524de8cfb5
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-05 21:02:00 +0530

    Time: 1 ms (29.11%) | Memory: 17.7 MB (62.03%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.py
@@ -4,12 +4,10 @@ class Solution:
         k=1
         while True:
             s=num1 - k*num2
-            if s < 0:
+            if s < 0 or k > s:
                 break
             if bin(s)[2:].count('1') <= k and k<=s:
                 return k
-            if k > s:
-                break
             k+=1
         return -1
 
