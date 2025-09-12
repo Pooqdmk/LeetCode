bd29ebeb0e07120e1136e49c4f395738723907d3
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-12 18:42:39 +0530

    Time: 85 ms (10.91%) | Memory: 18.1 MB (50.30%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -3,8 +3,14 @@ class Solution:
         
         n=0
         seen={'a','e','i','o','u'}
-        for i in s:
-            if i.lower() in seen:
+        left,right = 0,len(s)-1
+        
+        while left <= right:
+            if s[left] in seen:
                 n+=1
+            if s[right] in seen:
+                n+=1
+            left+=1
+            right-=1
         
         return False if n==0 else True
