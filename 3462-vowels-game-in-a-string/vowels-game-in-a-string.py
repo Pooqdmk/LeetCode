6f23c0957e4a35bbb7f11a0d7293217a0b4c75b2
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-12 18:44:57 +0530

    Time: 6 ms (84.85%) | Memory: 18.3 MB (31.82%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,16 +1,4 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         
-        n=0
-        seen={'a','e','i','o','u'}
-        left,right = 0,len(s)-1
-        
-        while left <= right:
-            if s[left] in seen:
-                n+=1
-            if s[right] in seen:
-                n+=1
-            left+=1
-            right-=1
-        
-        return False if n==0 else True
+        return False if len(set(s).intersection({'a','e','i','o','u'})) == 0 else True
