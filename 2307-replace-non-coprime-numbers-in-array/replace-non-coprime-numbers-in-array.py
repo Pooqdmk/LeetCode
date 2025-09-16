be57b35de8966bcd0cf9ef4c31e4494f8f9dc65f
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-16 22:51:14 +0530

    Time: 170 ms (31.52%) | Memory: 31.6 MB (36.96%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -2,13 +2,12 @@ class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         
         stack = []
-        i=0
-        while i < len(nums):
-            stack.append(nums[i])
+        
+        for i in nums:
+            stack.append(i)
             while len(stack) > 1 and gcd(stack[-1],stack[-2]) != 1:
                 v=stack.pop()
                 u=stack.pop()
                 stack.append(lcm(v,u))
                   
-            i+=1
         return stack
