f5baaf8bd4927b7ccfb7de938bca961e420f4e93
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-25 21:05:28 +0530

    Time: 811 ms (58.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+with cte as
+(
+    select turn,person_name,
+    sum(weight) over(order by turn) as tot_weight
+    from Queue
+)
+
+select person_name
+from cte
+where tot_weight <=1000
+order by turn desc
+limit 1
