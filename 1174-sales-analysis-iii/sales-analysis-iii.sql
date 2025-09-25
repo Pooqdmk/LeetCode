e78f42444c0357550c64d42eb00b605b4e31d096
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-25 20:05:36 +0530

    Time: 1303 ms (26.42%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,6 +1,13 @@
 SELECT p.product_id, p.product_name
 FROM Product p
-JOIN Sales s ON p.product_id = s.product_id 
-group by p.product_id
-having min(sale_date) >= '2019-01-01'
-and max(sale_date) <= '2019-03-31'
+where exists (
+    SELECT 1
+    FROM Sales s
+    WHERE s.product_id = p.product_id
+)
+and not exists (
+    select 1
+    from Sales s
+    where s.product_id = p.product_id
+    and (sale_date < '2019-01-01' or sale_date > '2019-03-31' )
+)
