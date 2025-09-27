207d3c77fd5b4fc245bf723f1e398971c49947c3
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-09-27 08:53:10 +0530

    Time: 51 ms (46.06%) | Memory: 18 MB (30.76%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.py
+++ 830-largest-triangle-area/largest-triangle-area.py
@@ -1,12 +1,23 @@
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
-        def area(p1,p2,p3):
-            x1,y1=p1
-            x2,y2=p2
-            x3,y3=p3
-            return .5* abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))
+        
+        # points.sort()
+        # x1,y1 = points[0][0],points[0][1]
+        # x3,y3 = points[-1][0] ,points[-1][1]
+        # points.remove([x1,y1])
+        # points.remove([x3,y3])
+        # points.sort(key = lambda x : x[1],reverse = True)
+        # x2,y2 = points[0][0],points[0][1]
 
-        max_area=0
+        # return .5*abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1-y2))
+
+        def area(p1,p2,p3):
+            x1,y1 = p1
+            x2,y2 = p2
+            x3,y3 = p3
+            return .5*abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1-y2))
+        mx= 0
+
         for p1,p2,p3 in combinations(points,3):
-            max_area=max(max_area,area(p1,p2,p3))
-        return max_area
+            mx = max(mx, area(p1,p2,p3))
+        return mx
