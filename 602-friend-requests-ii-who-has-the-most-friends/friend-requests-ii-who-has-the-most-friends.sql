d41dc9abb2de85f7e048dda8e8dece9bee93c955
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-10-02 18:32:53 +0530

    Time: 292 ms (70.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+with cte as
+(
+select requester_id as id
+from RequestAccepted
+union all
+select accepter_id
+from RequestAccepted
+)
+
+
+select id,count(*) as num
+from cte
+group by id
+order by num desc
+limit 1
