324b09383fc92c645421d86d15e9ab34c6f5feb4
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-10-04 09:09:50 +0530

    Time: 107 ms (37.84%) | Memory: 28.5 MB (68.11%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,9 +3,8 @@ class Solution:
         left,right =0,len(height)-1
         mx = -1
         while left < right:
-            mn = min(height[left],height[right])
-            mx = max(mx, mn * (right - left))
-            if mn == height[left]:
+            mx = max(mx, min(height[left],height[right]) * (right - left))
+            if height[right] > height[left]:
                 left+=1
             else:
                 right-=1
