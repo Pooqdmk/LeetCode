752bbd1aa56532411e6fafc2966e97f0a0e26564
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-10-15 19:04:06 +0530

    Time: 4 ms (63.61%) | Memory: 18.1 MB (5.91%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        d = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M',4:'IV', 9:'IX', 40:'XL', 90:'XC', 400:'CD', 900:'CM'}
+        l = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        res = []
+        for i in l:
+            if num >= i:
+                res.append(d[i]*(num//i))
+                num = num%i
+        return ''.join(res)
+
+                    
