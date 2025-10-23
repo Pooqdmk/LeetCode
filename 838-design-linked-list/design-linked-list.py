3327afd55b2082474d1ccd39135da088741f3863
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-10-23 21:05:15 +0530

    Time: 85 ms (6.77%) | Memory: 18.6 MB (78.25%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self,val = 0,next = None):
+        self.next = next
+        self.val = val
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.l = Node()
+
+    def get(self, index: int) -> int:
+        cur = self.l.next
+        i = 0
+        while cur:
+            if i == index:
+                return cur.val
+            i+=1
+            cur = cur.next
+        return -1
+        
+
+    def addAtHead(self, val: int) -> None:
+        newNode = Node(val = val,next = self.l.next)
+        self.l.next = newNode
+
+
+    def addAtTail(self, val: int) -> None:
+        newNode = Node(val = val)
+        cur = self.l
+        while cur and cur.next:
+            cur = cur.next
+        cur.next = newNode
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        newNode = Node(val = val)
+        i = 0
+        
+        cur = self.l.next
+        prev = self.l
+        while cur:
+            if i == index:
+                prev.next = newNode
+                newNode.next = cur
+                break
+            i+=1
+            prev = cur
+            cur = cur.next
+        if i == index:
+            prev.next = newNode
+
+    def deleteAtIndex(self, index: int) -> None:
+        i = 0
+        
+        cur = self.l.next
+        prev = self.l
+        while cur:
+            if i == index:
+                prev.next = cur.next
+                break
+            i+=1
+            prev = cur
+            cur = cur.next
+
+        
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
