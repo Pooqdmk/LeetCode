7cec30114c7ed9bf59730ee13841e34002afaf41
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-10-23 08:03:26 +0530

    Time: 87 ms (30.16%) | Memory: 18 MB (26.37%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -1,12 +1,13 @@
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
-        while len(s) > 2:
-            res=[]
+        
+        while len(s)!=2:
+            res = []
             for i in range(0,len(s)-1):
                 res.append(str((int(s[i])+int(s[i+1]))%10))
+
+            s = ''.join(res)
             
-            s=''.join(res)
-
-        return s[0] == s[1]
-
+        return True if s[0] == s[1] else False
                 
+            
