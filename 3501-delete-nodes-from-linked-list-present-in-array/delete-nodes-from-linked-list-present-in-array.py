ebfd014f04b1f665d56d802d92695e2e3287ec4d
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-01 18:42:35 +0530

    Time: 47 ms (96.15%) | Memory: 58.5 MB (79.49%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -5,15 +5,16 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        prev = dummy
-
-        cur = head
+        dummy = ListNode(next = head)
+        prev,cur = dummy,head
         seen = set(nums)
         while cur:
-            if cur.val not in seen:
-                prev.next = ListNode(val = cur.val)
-                prev = prev.next
-            cur = cur.next
+            if cur.val in seen:
+                prev.next = cur.next
+                cur = cur.next
+            else:
+                prev = cur
+                cur = cur.next
         return dummy.next
-            
+
+
