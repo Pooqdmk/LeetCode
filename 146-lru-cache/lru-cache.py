cd6492a59a5342a6b6cd0198637ff0b1bfebd2f1
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-07 13:56:58 +0530

    Time: 174 ms (19.09%) | Memory: 77.9 MB (71.81%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,49 @@
+class Node:
+    def __init__(self,val = 0,key = 0):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cap = capacity
         self.d = {}
-        self.capacity = capacity
-        self.q = deque()
-        self.seen = set()
+        self.left = Node()
+        self.right = Node()
+        self.left.next = self.right
+        self.right.prev = self.left
+    def delete(self,node):
+        prev,nxt = node.prev,node.next
+        prev.next = nxt
+        nxt.prev = prev
+    def insert(self,node):
+        prev = self.right.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.right
+        self.right.prev = node
     def get(self, key: int) -> int:
-        if key in self.seen:
-            self.q.remove(key)
-            self.q.append(key)
-            return self.d[key]
+        if key in self.d:
+            self.delete(self.d[key])
+            self.insert(self.d[key])
+            return self.d[key].val
         return -1
-        
+
     def put(self, key: int, value: int) -> None:
-        if key in self.seen:
-            self.d[key] = value
-            self.q.remove(key)
-            
-        elif self.capacity == len(self.d):
-            r = self.q.popleft()
-            del self.d[r] 
-            self.seen.remove(r)   
-        self.seen.add(key)
-        self.q.append(key)
-        self.d[key] = value  
-        
+        if key in self.d:
+            self.delete(self.d[key])
+        self.d[key] = Node(key = key,val = value)
+        self.insert(self.d[key])
+        if len(self.d) > self.cap:
+            n_del = self.left.next
+            self.delete(n_del)
+            del self.d[n_del.key]
+
 
 # Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity) 
+# obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
