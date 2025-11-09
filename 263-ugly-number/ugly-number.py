3e990c628734e4e7a60f9de4bebb64b1ea318fa0
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-09 18:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.83%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -2,20 +2,25 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n<=0:
             return False
-        factors = set()
-        while n%2 == 0:
-            factors.add(2)
-            n//=2
+        # factors = set()
+        # while n%2 == 0:
+        #     factors.add(2)
+        #     n//=2
 
-        for i in range(3,int(sqrt(n))+1,2):
+        # for i in range(3,int(sqrt(n))+1,2):
+        #     while n%i == 0:
+        #         factors.add(i)
+        #         n//=i
+        # if n>2:
+        #     factors.add(n)
+        
+        # for i in factors:
+        #     if i not in [2,3,5]:
+        #         return False
+        # return True
+        
+
+        for i in [2,3,5]:
             while n%i == 0:
-                factors.add(i)
                 n//=i
-        if n>2:
-            factors.add(n)
-        
-        for i in factors:
-            if i not in [2,3,5]:
-                return False
-        return True
-        
+        return n == 1
