3ac31f2240fab73ca5b2e40c8c435e0e42d4a7b8
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-20 10:33:41 +0530

    Time: 15 ms (31.03%) | Memory: 18.9 MB (79.31%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -1,16 +1,14 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda x:(x[-1],-x[0]))
-        print(intervals)
-        res = 2
-        p1,p2 = intervals[0][-1]-1, intervals[0][-1]  
-        for i in range(1,len(intervals)):
-            if p2 < intervals[i][0]:
-                p1,p2 = intervals[i][-1]-1,intervals[i][-1]
-                res+=2        
-            elif p1<intervals[i][0]:
-                p1 = p2
-                p2 = intervals[i][-1]
-                res+=1
-        return res
+        res = 0
+        p1,p2=-1,-1
 
+        for i,j in intervals:
+            if p2 < i:
+                res+=2
+                p1,p2 = j-1,j
+            elif p1 < i:
+                res+=1
+                p1,p2 = p2,j
+        return res
