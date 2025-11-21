fc3f78da7c8251a3ab595a76ca72dbf9fb7a3a57
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-21 22:31:46 +0530

    Time: 625 ms (62.81%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,12 +1,14 @@
 with cte as
 (
-select d.id as id,d.name as name,max(salary) as max_salary
-from Employee e
-join Department d
-on e.departmentId = d.id
-group by d.id
+    select d.id as id,d.name as name, max(salary) as salary
+    from Employee e
+    join Department d
+    on e.departmentId = d.id
+    group by d.id
 )
-select cte.name as Department, e.name as Employee,e.salary as Salary 
-from cte
+
+select cte.name as Department, e.name as Employee, e.salary
+from cte 
 join Employee e
-on cte.id = e.departmentId and e.salary = max_salary
+on e.departmentId = cte.id
+where e.salary = cte.salary
