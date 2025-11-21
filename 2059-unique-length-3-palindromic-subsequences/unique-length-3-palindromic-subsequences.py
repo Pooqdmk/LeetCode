472110f33a0dcc0849b71411e1ced2bbc3b0e752
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-21 09:26:23 +0530

    Time: 2200 ms (14.83%) | Memory: 18.2 MB (59.85%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -2,13 +2,13 @@ class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         left,right = set(),Counter(s)
         cnt = 0
-        seen = set()
+        seen = {}
         for i in range(len(s)):
             right[s[i]]-=1
             if len(left)>0:
                 for j in left:
                     if j in right and right[j]>0 and j+s[i] not in seen:
-                        seen.add(j+s[i])
+                        seen[j+s[i]] = 1
                         cnt+=1
             left.add(s[i])
             
