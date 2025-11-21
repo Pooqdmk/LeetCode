3969fad99bae7e89714a7237a376ef7e7f22ae73
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-21 09:39:32 +0530

    Time: 1744 ms (21.48%) | Memory: 18.3 MB (59.85%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -4,16 +4,15 @@ class Solution:
         for i in s:
             right[i] = right.get(i,0)+1
         left = set()
-        cnt = 0
-        seen = {}
+        seen = set()
         for i in range(len(s)):
             right[s[i]]-=1
             if len(left)>0:
                 for j in left:
                     if j in right and right[j]>0 and j+s[i] not in seen:
-                        seen[j+s[i]] = 1
-                        cnt+=1
+                        seen.add(j+s[i])
+                        
             left.add(s[i])
             
-        return cnt
+        return len(seen)
             
