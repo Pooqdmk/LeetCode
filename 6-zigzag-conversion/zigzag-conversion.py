73359ba4f1e5bc4e842871d2f487679d5ab555ce
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-11-26 09:22:56 +0530

    Time: 15 ms (37.28%) | Memory: 17.7 MB (89.59%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        res = []
+        for i in range(numRows):
+            incr = (numRows-1)*2
+            for j in range(i,len(s),incr):
+                res.append(s[j])
+
+                if i>0 and i<numRows-1 and j+incr-2*i < len(s):
+                    res.append(s[j+incr-2*i])
+        return ''.join(res)
