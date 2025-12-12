2543caaee1b162b826a651c4857abdc6f15232e2
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-12-12 21:08:41 +0530

    Time: 434 ms (25.13%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,13 +1,9 @@
-# Write your MySQL query statement below
-#scalar sub query returns a null if nothing is given out by hte inner sub query 
-#with a single select it nothin is is given by the limit then it prints nothing
-
-select
+with cte as
 (
-select *
+select num
 from MyNumbers
 group by num
-having count(num) = 1
-order by num desc
-limit 1
-) as num
+having count(*) = 1
+)
+
+select max(num) as num from cte
