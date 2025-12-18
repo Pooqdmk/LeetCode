69671a033fa454721a1db353613c80fe5b1428a1
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2025-12-18 19:50:44 +0530

    Time: 294 ms (60.32%) | Memory: 33.6 MB (90.39%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,26 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
         n = len(prices)
-        pre = [0]*n
-        pre[0] = prices[0]*strategy[0]
-        l = [0]*n
-        l[0] = prices[0]
-        for i in range(1,n):
-            pre[i] = pre[i-1] + (prices[i]*strategy[i])
-            l[i] = l[i-1]+prices[i]
-        res = -10**60
-        for i in range(k-1,n):
-            if i-k >= 0:
-                left = pre[i-k]
-            else:
-                left = 0
-            right = pre[-1] - pre[i]
-            if i-k // 2 >= 0:
-                curr = l[i] - l[i-(k//2)]
-            else:
-                curr = 0
-            res = max(res, left+right+curr)
-        return max(res, pre[-1])
+        pre = [0]*(n+1)
+        l = [0]*(n+1)
 
+        for i in range(1,n+1):
+            pre[i] = pre[i-1] + (prices[i-1]*strategy[i-1])
+            l[i] = l[i-1] + prices[i-1]
+        # print(pre, l)
+        gain = 0
+        original = pre[-1]
+        for r in range(k,n+1):
+            left = r-k
+            mid = k//2 + left
+            new_win = l[r] - l[mid]
+            old_win = pre[r] - pre[left]
 
-        
+            gain = max(gain, new_win - old_win)
+        return original + gain
