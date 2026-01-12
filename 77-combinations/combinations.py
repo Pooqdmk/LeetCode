ffdf1666b0ea054cb5e738c6272deb66d45d6848
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2026-01-12 11:05:04 +0530

    Time: 79 ms (95.98%) | Memory: 61.6 MB (9.02%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,22 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res,sol =[],[]
-        def dfs(i):
+        
+        res, sol = [],[]
+
+        def dfs(n):
             if len(sol) == k:
                 res.append(sol[:])
-                return
+                return 
             
-            for j in range(i,n):
-                if j+1 not in sol:
-                    sol.append(j+1)
-                    dfs(j+1)
-                    sol.pop()
+            left = n
+            need = k - len(sol) 
+            if left > need:
+                dfs(n-1)
             
-        dfs(0)
+            sol.append(n)
+            dfs(n-1)
+            sol.pop()
+        dfs(n)
         return res
+        
+
