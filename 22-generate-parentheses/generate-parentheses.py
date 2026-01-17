a1aca7f5266d540963504daff30dec471040561d
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2026-01-17 12:35:30 +0530

    Time: 4 ms (13.87%) | Memory: 19.4 MB (18.80%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res,sol = [],[]
+        
+        def dfs(i,open,close):
+            if len(sol[:]) == 2*n:
+                res.append(''.join(sol[:]))
+                return
+            
+            if open < n:
+                sol.append('(')
+                dfs(i+1,open+1,close)
+                sol.pop()
+            if open > close:
+                sol.append(')')
+                dfs(i+1,open,close+1)
+                sol.pop()
+        dfs(1,0,0)
+        return res
+
+
+
+            
