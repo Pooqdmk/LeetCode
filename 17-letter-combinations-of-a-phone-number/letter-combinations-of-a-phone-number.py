df5a7a2d8d32d5ecd0ba80850ab33f1f34c84ab5
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2026-01-17 10:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (7.77%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,26 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        n = len(digits)
-        res = []
-        d = {
-            '2' : 'abc',
-            '3' : 'def',
-            '4' : 'ghi',
-            '5' : 'jkl',
-            '6' : 'mno',
-            '7' : 'pqrs',
-            '8' : 'tuv',
-            '9' : 'wxyz'
-        }
+        
+        d = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
 
-        def backtrack(i, curr):
-            if len(curr) == n:
-                res.append(curr)
+        res,sol = [],[]
+        n = len(digits)
+
+        def dfs(i):
+            if i == n:
+                res.append(''.join(sol[:]))
                 return 
-            
+
             for c in d[digits[i]]:
-                backtrack(i+1, curr+c)
-        if digits:
-            backtrack(0,'')
+                sol.append(c)
+                dfs(i+1)
+                sol.pop()
+        dfs(0)
+
         return res
         
