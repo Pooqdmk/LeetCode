2127e84c342f8603a923da68a3cd8051b4b34d2e
Poorvi Srikanth <127005010+Pooqdmk@users.noreply.github.com>
2026-02-01 10:32:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.15%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,3 +1,15 @@
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        return nums[0]+sum(sorted(nums[1:])[:2])
+        e = nums[0]
+        s=e
+        nums.sort()
+        if nums[0] != e:
+            s+=nums[0]
+        else:
+            s+=nums[1]+nums[2]
+            return s
+        if nums[1]!= e:
+            s+=nums[1]
+        else:
+            s+=nums[2]
+        return s
